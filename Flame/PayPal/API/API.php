642a3f5d635150a1edf6ce6b6b39de6ab4d1d962
František Maša <frantisekmasa1@gmail.com>
2015-03-26 21:31:26 +0100

    [curl] Updated deprecated value

--- Flame/PayPal/API/API.php
+++ Flame/PayPal/API/API.php
@@ -292,7 +292,7 @@ class API extends Object
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 		// We should check if paypal has valid certificate
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 		// Just do normal POST
